1f3439d1e9b5ed1681393054f7cca6211170714d
Mitchell Katz <sk1er@lythrim.net>
2019-03-07 22:03:22 -0500

    Fix crashing

--- src/main/java/club/sk1er/mods/levelhead/Levelhead.java
+++ src/main/java/club/sk1er/mods/levelhead/Levelhead.java
@@ -51,7 +51,7 @@ public class Levelhead extends DummyModContainer {
         Hello !
      */
     public static final String MODID = "LEVEL_HEAD";
-    public static final String VERSION = "6.2";
+    public static final String VERSION = "6.3";
     private static Levelhead instance;
     public UUID userUuid = null;
     public int count = 1;
