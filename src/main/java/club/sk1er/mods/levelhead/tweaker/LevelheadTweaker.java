d68c1dd05f5e6c5034103f60e875adcbab937cf1
asbyth <cyronize@gmail.com>
2021-05-25 07:49:53 -0400

    change: update loader & essential

--- src/main/java/club/sk1er/mods/levelhead/tweaker/LevelheadTweaker.java
+++ src/main/java/club/sk1er/mods/levelhead/tweaker/LevelheadTweaker.java
@@ -1,10 +1,10 @@
 package club.sk1er.mods.levelhead.tweaker;
 
 import club.sk1er.mods.levelhead.forge.FMLLoadingPlugin;
-import gg.essential.loader.EssentialTweaker;
+import gg.essential.loader.EssentialSetupTweaker;
 
 @SuppressWarnings("unused")
-public class LevelheadTweaker extends EssentialTweaker {
+public class LevelheadTweaker extends EssentialSetupTweaker {
     public LevelheadTweaker() {
         super(new String[]{FMLLoadingPlugin.class.getName()});
     }
