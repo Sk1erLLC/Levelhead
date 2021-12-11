d8097421af8dc1e8c3797161088d0dd1c0fb03d4
Sychic <47618543+Sychic@users.noreply.github.com>
2021-12-11 00:51:44 -0500

    TabRender: Check for self showing

--- src/main/kotlin/club/sk1er/mods/levelhead/render/TabRender.kt
+++ src/main/kotlin/club/sk1er/mods/levelhead/render/TabRender.kt
@@ -2,14 +2,16 @@ package club.sk1er.mods.levelhead.render
 
 import club.sk1er.mods.levelhead.Levelhead
 import gg.essential.universal.UMinecraft
+import gg.essential.universal.wrappers.UPlayer
 import net.minecraft.client.network.NetworkPlayerInfo
 
 object TabRender {
 
     fun drawPingHook(offset: Int, x: Int, y: Int, playerInfo: NetworkPlayerInfo) {
         if (!Levelhead.displayManager.config.enabled) return
-        Levelhead.displayManager.tab?.run {
-            if (!this.config.enabled || !Levelhead.LevelheadPurchaseStates.tab) return
+        Levelhead.displayManager.tab.run {
+            if (!this.config.enabled || !Levelhead.LevelheadPurchaseStates.tab ||
+                (playerInfo.gameProfile.id == UPlayer.getUUID() && !this.config.showSelf)) return
 
             this.cache[playerInfo.gameProfile.id]?.footer?.value?.let { str ->
                 var x1 = offset + x - 12 - UMinecraft.getFontRenderer().getStringWidth(str)
@@ -31,9 +33,9 @@ object TabRender {
 
     fun getLevelheadWidth(playerInfo: NetworkPlayerInfo)  = when {
         !Levelhead.displayManager.config.enabled -> 0
-        (Levelhead.displayManager.tab?.config?.enabled == true &&
+        (Levelhead.displayManager.tab.config.enabled == true &&
          Levelhead.LevelheadPurchaseStates.tab) -> {
-                UMinecraft.getFontRenderer().getStringWidth(Levelhead.displayManager.tab!!.cache[playerInfo.gameProfile.id]?.footer?.value) + 2
+                UMinecraft.getFontRenderer().getStringWidth(Levelhead.displayManager.tab.cache[playerInfo.gameProfile.id]?.footer?.value) + 2
             }
         else -> 0
     }
