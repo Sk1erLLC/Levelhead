8b24c9fde03e74c43652beea1250196225022dfb
Sychic <47618543+Sychic@users.noreply.github.com>
2021-12-22 07:22:26 -0500

    AboveHeadRender: Use UC

--- src/main/kotlin/club/sk1er/mods/levelhead/render/AboveHeadRender.kt
+++ src/main/kotlin/club/sk1er/mods/levelhead/render/AboveHeadRender.kt
@@ -6,13 +6,13 @@ import club.sk1er.mods.levelhead.display.LevelheadTag
 import club.sk1er.mods.levelhead.gui.LevelheadGUI
 import gg.essential.api.EssentialAPI
 import gg.essential.elementa.utils.withAlpha
+import gg.essential.universal.UGraphics
+import gg.essential.universal.UMatrixStack
 import gg.essential.universal.UMinecraft
 import gg.essential.universal.UMinecraft.getFontRenderer
 import gg.essential.universal.UMinecraft.getMinecraft
 import gg.essential.universal.wrappers.UPlayer
 import net.minecraft.client.gui.FontRenderer
-import net.minecraft.client.renderer.GlStateManager
-import net.minecraft.client.renderer.Tessellator
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats
 import net.minecraft.entity.EntityLivingBase
 import net.minecraft.entity.player.EntityPlayer
@@ -57,7 +57,7 @@ object AboveHeadRender {
     private fun renderName(tag: LevelheadTag, entityIn: EntityPlayer, x: Double, y: Double, z: Double) {
         val fontrenderer = getFontRenderer()
         val textScale = 0.016666668f * 1.6f * displayManager.config.fontSize
-        GlStateManager.pushMatrix()
+        UGraphics.GL.pushMatrix()
         val mc = getMinecraft()
         val xMultiplier = if (
             mc.gameSettings?.let { it.thirdPersonView == 2 } == true
@@ -66,33 +66,29 @@ object AboveHeadRender {
         } else {
             1
         }
-        GlStateManager.translate(x.toFloat() + 0.0f, y.toFloat() + entityIn.height + 0.5f, z.toFloat())
+        UGraphics.GL.translate(x.toFloat() + 0.0f, y.toFloat() + entityIn.height + 0.5f, z.toFloat())
         GL11.glNormal3f(0.0f, 1.0f, 0.0f)
         val renderManager = mc.renderManager
-        GlStateManager.rotate(-renderManager.playerViewY, 0.0f, 1.0f, 0.0f)
-        GlStateManager.rotate(renderManager.playerViewX * xMultiplier, 1.0f, 0.0f, 0.0f)
-        GlStateManager.scale(-textScale, -textScale, textScale)
-        GlStateManager.disableLighting()
-        GlStateManager.depthMask(false)
-        GlStateManager.disableDepth()
-        GlStateManager.enableBlend()
-        GlStateManager.tryBlendFuncSeparate(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, GL11.GL_ONE, GL11.GL_ZERO)
-        val tessellator = Tessellator.getInstance()
-        val worldrenderer = tessellator.worldRenderer
+        UGraphics.GL.rotate(-renderManager.playerViewY, 0.0f, 1.0f, 0.0f)
+        UGraphics.GL.rotate(renderManager.playerViewX * xMultiplier, 1.0f, 0.0f, 0.0f)
+        UGraphics.GL.scale(-textScale, -textScale, textScale)
+        UGraphics.disableLighting()
+        UGraphics.depthMask(false)
+        UGraphics.disableDepth()
+        UGraphics.enableBlend()
+        UGraphics.tryBlendFuncSeparate(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, GL11.GL_ONE, GL11.GL_ZERO)
         val stringWidth = fontrenderer.getStringWidth(tag.getString()) shr 1
-        GlStateManager.disableTexture2D()
-        worldrenderer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_COLOR)
-        worldrenderer.pos((-stringWidth - 1).toDouble(), -1.0, 0.0).color(0.0f, 0.0f, 0.0f, 0.25f).endVertex()
-        worldrenderer.pos((-stringWidth - 1).toDouble(), 8.0, 0.0).color(0.0f, 0.0f, 0.0f, 0.25f).endVertex()
-        worldrenderer.pos((stringWidth + 1).toDouble(), 8.0, 0.0).color(0.0f, 0.0f, 0.0f, 0.25f).endVertex()
-        worldrenderer.pos((stringWidth + 1).toDouble(), -1.0, 0.0).color(0.0f, 0.0f, 0.0f, 0.25f).endVertex()
-        tessellator.draw()
-        GlStateManager.enableTexture2D()
+        val uGraphics = UGraphics.getFromTessellator().beginWithDefaultShader(UGraphics.DrawMode.QUADS, DefaultVertexFormats.POSITION_COLOR)
+        uGraphics.pos(UMatrixStack.Compat.get(), (-stringWidth - 1).toDouble(), -1.0, 0.0).color(0.0f, 0.0f, 0.0f, 0.25f).endVertex()
+        uGraphics.pos(UMatrixStack.Compat.get(), (-stringWidth - 1).toDouble(), 8.0, 0.0).color(0.0f, 0.0f, 0.0f, 0.25f).endVertex()
+        uGraphics.pos(UMatrixStack.Compat.get(), (stringWidth + 1).toDouble(), 8.0, 0.0).color(0.0f, 0.0f, 0.0f, 0.25f).endVertex()
+        uGraphics.pos(UMatrixStack.Compat.get(), (stringWidth + 1).toDouble(), -1.0, 0.0).color(0.0f, 0.0f, 0.0f, 0.25f).endVertex()
+        uGraphics.drawDirect()
         renderString(fontrenderer, tag)
-        GlStateManager.enableLighting()
-        GlStateManager.disableBlend()
-        GlStateManager.color(1.0f, 1.0f, 1.0f, 1.0f)
-        GlStateManager.popMatrix()
+        UGraphics.enableLighting()
+        UGraphics.disableBlend()
+        UGraphics.color4f(1.0f, 1.0f, 1.0f, 1.0f)
+        UGraphics.GL.popMatrix()
     }
 
     private fun renderString(renderer: FontRenderer, tag: LevelheadTag) {
@@ -105,22 +101,22 @@ object AboveHeadRender {
     }
 
     private fun render(renderer: FontRenderer, component: LevelheadTag.LevelheadComponent, x: Int) {
-        GlStateManager.disableDepth()
-        GlStateManager.depthMask(true)
-        GlStateManager.disableDepth()
-        GlStateManager.depthMask(false)
+        UGraphics.disableDepth()
+        UGraphics.depthMask(true)
+        UGraphics.disableDepth()
+        UGraphics.depthMask(false)
         if (component.chroma) {
             renderer.drawString(component.value, x, 0, Levelhead.DarkChromaColor)
         } else {
             renderer.drawString(component.value, x, 0, component.color.withAlpha(0.2f).rgb)
         }
-        GlStateManager.enableDepth()
-        GlStateManager.depthMask(true)
-        GlStateManager.color(1.0f, 1.0f, 1.0f)
+        UGraphics.enableDepth()
+        UGraphics.depthMask(true)
+        UGraphics.directColor3f(1.0f, 1.0f, 1.0f)
         if (component.chroma) {
             renderer.drawString(component.value, x, 0, Levelhead.ChromaColor)
         } else {
-            GlStateManager.color(
+            UGraphics.color4f(
                 component.color.red / 255f,
                 component.color.green / 255f,
                 component.color.blue / 255f,
