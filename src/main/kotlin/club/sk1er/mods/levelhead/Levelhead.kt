2df09c521bd7a41978d0d6d8389598c6c2cc6293
Sychic <47618543+Sychic@users.noreply.github.com>
2021-12-29 23:26:25 -0500

    Release: Levelhead 8.0

--- src/main/kotlin/club/sk1er/mods/levelhead/Levelhead.kt
+++ src/main/kotlin/club/sk1er/mods/levelhead/Levelhead.kt
@@ -84,7 +84,7 @@ object Levelhead {
         get() = displayManager.aboveHead[0].cache[UPlayer.getUUID()]!!
 
     const val MODID = "level_head"
-    const val VERSION = "8.0.0-RC1"
+    const val VERSION = "8.0.0"
 
     @Mod.EventHandler
     fun preInit(event: FMLPreInitializationEvent) {
