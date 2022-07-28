b652ae811f009e33470a6d45a2fd36739e7a107e
Sychic <47618543+Sychic@users.noreply.github.com>
2022-07-28 14:52:14 -0400

    SliderComponent: Remove usage of internal vigilance things and refactor to settings package

---
+++ src/main/kotlin/club/sk1er/mods/levelhead/gui/components/settings/SliderComponent.kt
@@ -0,0 +1,78 @@
+package club.sk1er.mods.levelhead.gui.components.settings
+
+import gg.essential.elementa.components.UIContainer
+import gg.essential.elementa.components.UIText
+import gg.essential.elementa.constraints.*
+import gg.essential.elementa.constraints.animation.Animations
+import gg.essential.elementa.dsl.*
+import gg.essential.elementa.font.DefaultFonts
+import gg.essential.universal.USound
+import gg.essential.vigilance.gui.VigilancePalette
+import gg.essential.vigilance.gui.settings.Slider
+import gg.essential.vigilance.utils.onLeftClick
+import kotlin.math.roundToInt
+
+class SliderComponent(initialValue: Int, min: Int, max: Int) : UIContainer() {
+    private var onValueChange: (Int) -> Unit = {}
+    private var lastValue: Int? = null
+    private var expanded = false
+    private var mouseHeld = false
+
+    private val minText by UIText(min.toString()).constrain {
+        y = CenterConstraint()
+        color = VigilancePalette.getMidText().constraint
+        fontProvider = DefaultFonts.VANILLA_FONT_RENDERER
+    } childOf this
+
+    private val slider by Slider((initialValue.toFloat() - min) / (max - min)).constrain {
+        x = SiblingConstraint()
+        width = 60.pixels()
+        height = 12.pixels()
+    } childOf this
+
+    private val maxText by UIText(max.toString()).constrain {
+        x = SiblingConstraint()
+        y = CenterConstraint()
+        color = VigilancePalette.getMidText().constraint
+        fontProvider = DefaultFonts.VANILLA_FONT_RENDERER
+    } childOf this
+
+    private val currentValueText by UIText(initialValue.toString()).constrain {
+        x = CenterConstraint()
+        y = CenterConstraint()
+        color = VigilancePalette.getMidText().constraint
+        fontProvider = DefaultFonts.VANILLA_FONT_RENDERER
+    } childOf slider.grabBox
+
+    init {
+        slider.onValueChange { newPercentage ->
+            val newValue = (min + (newPercentage * (max - min))).roundToInt()
+            if (newValue != lastValue) {
+                this.onValueChange(newValue)
+            }
+            currentValueText.setText(newValue.toString())
+        }
+
+        onLeftClick {
+            USound.playButtonPress()
+            mouseHeld = true
+        }
+        onMouseRelease {
+            mouseHeld = false
+            if (expanded && !slider.isHovered()) {
+                slider.animate {
+                    setWidthAnimation(Animations.OUT_EXP, .25f, 60.pixels())
+                }
+                expanded = false
+            }
+        }
+        constrain {
+            width = ChildBasedSizeConstraint()
+            height = ChildBasedMaxSizeConstraint()
+        }
+    }
+
+    fun onValueChange(listener: (Int) -> Unit) = apply {
+        this.onValueChange = listener
+    }
+}
